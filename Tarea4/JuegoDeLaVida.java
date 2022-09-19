3a1d75ec6e32aa7e57ac248915037c9d6cc69b1f
LeoGaribay <111948874+LeoGaribay@users.noreply.github.com>
2022-09-18 23:15:42 -0500

    Create JuegoDeLaVida.java

---
+++ Tarea4/JuegoDeLaVida.java
@@ -0,0 +1,28 @@
+package Juego;
+
+import ADT.Arreglo2DADT;
+
+public class JuegoDeLaVida {
+	public static Arreglo2DADT tablero;
+	public static Arreglo2DADT temporal;
+
+	public static void main(String[] args) {
+
+		tablero = new Arreglo2DADT<Integer>(15, 15);
+		tablero.limpiar(0); //rellenar de 0s
+		tablero.set_Elemento(2, 2, 1);
+		tablero.set_Elemento(2, 3, 1);
+		tablero.set_Elemento(2, 4, 1);
+		tablero.set_Elemento(6, 2, 1);
+		tablero.set_Elemento(6, 3, 1);
+		tablero.set_Elemento(6, 4, 1);
+		tablero.set_Elemento(6, 5, 1);		
+		Tablero.imprimirTableroInicial();
+		for (int i = 1; i < 56; i++) {
+			Tablero.calcularGeneraciones(temporal, tablero.get_Filas(), tablero.get_Columnas()); // imprimir las veces que se especifiquen
+			int numero= i+1;
+			System.out.println("Generación número " + numero);
+		}
+
+	}
+}
